29047bc75200443cedb7d084d49c3a693851031b
Willis Wang <wwang4292@gmail.com>
2026-08-11 02:24:06 -0700

    Use Snacks Explorer for files

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -1,27 +1,26 @@
 return {
   {
-    "stevearc/oil.nvim",
-    lazy = false,
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-      skip_confirm_for_simple_edits = true,
-      view_options = { show_hidden = true },
-    },
-    keys = {
-      { "<leader>e", "<cmd>Oil<cr>", desc = "File explorer" },
-      { "<C-n>", "<cmd>Oil<cr>", desc = "File explorer (legacy)" },
-    },
-  },
-  {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
+      explorer = { enabled = true },
       input = { enabled = true },
       notifier = { enabled = true },
-      picker = { enabled = true },
+      picker = {
+        enabled = true,
+        sources = {
+          explorer = {
+            git_status = true,
+            watch = true,
+          },
+        },
+      },
     },
     keys = {
+      { "<leader>e", function() Snacks.explorer() end, desc = "File explorer" },
+      { "<C-n>", function() Snacks.explorer() end, desc = "File explorer (legacy)" },
       { "<leader>f", function() Snacks.picker.files() end, desc = "Find files" },
       { "<leader>s", function() Snacks.picker.grep() end, desc = "Search text" },
       { "<leader>b", function() Snacks.picker.buffers() end, desc = "Find buffers" },
